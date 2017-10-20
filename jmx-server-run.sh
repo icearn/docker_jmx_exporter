386f1c0d94b0828054fca2b0ed679609d42c933a
icearn <32473321+icearn@users.noreply.github.com>
2017-10-20 15:03:40 +1300

    Create jmx-server-run.sh

---
+++ jmx-server-run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java $HEAP_OPTS -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=$REMOTE_PORT -jar jmx_prometheus_httpserver.jar
