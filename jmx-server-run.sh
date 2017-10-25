1bad8270831c9c95fe303118b5b38ad7d4376393
icearn <32473321+icearn@users.noreply.github.com>
2017-10-25 16:10:45 +1300

    Update jmx-server-run.sh

--- jmx-server-run.sh
+++ jmx-server-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java $HEAP_OPTS -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=$REMOTE_PORT -jar jmx_prometheus_httpserver.jar $SERVICE_PORT /opt/jmx_exporter/conf/jmx-exporter.yaml
+java $HEAP_OPTS -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=$REMOTE_PORT -jar jmx_prometheus_httpserver.jar $SERVER_PORT /opt/jmx_exporter/conf/jmx-exporter.yaml
